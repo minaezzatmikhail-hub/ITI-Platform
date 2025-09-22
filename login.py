bc030b02050eaa1ced3983749bd09a792673fc6a
minaezzatmikhail-hub <minaezzatmikhail@gmail.com>
2025-09-23 00:28:39 +0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,7 @@
+username = input("Username: ")
+password = input("Password: ")
+
+if username == "Mina" and password == "1234":
+    print("Welcome To ITI Platform")
+else:
+    print("Wrong username or password")
